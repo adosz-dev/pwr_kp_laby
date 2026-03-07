4cc7710ec2bb1da7285166a4779150d0da2af656
Adam Latos-Ważny <adamlw06@protonmail.com>
2026-03-07 11:14:41 +0100

    lista 0

---
+++ lista0/zadanie3/zadanie3.java
@@ -0,0 +1,22 @@
+public class zadanie3 {
+  public static int div(int n){
+    int m = 0;
+    for (int i=1; i<n; i++){
+      if (n % i == 0){
+        m = i;
+      } 
+    }
+    return m;
+  }
+  public static void main(String[] args){
+    for (int i=0; i<args.length; i++){
+      try {
+        int n = Integer.parseInt(args[i]);
+        System.out.println(args[i] + " — największy dzielnik: " + div(n));
+      }
+      catch (NumberFormatException ex) {
+        System.out.println(args[i] + " nie jest liczbą całkowitą");
+      }
+    }
+  }
+}
