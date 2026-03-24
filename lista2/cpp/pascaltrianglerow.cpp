801015c8223dbe99167151bae31c889daa3bd3bc
Adam Latos-Wa??ny <adamlw06@protonmail.com>
2026-03-24 12:45:57 +0100

    lista 2 cpp wyrzucenie petli z konstruktora

--- lista2/cpp/pascaltrianglerow.cpp
+++ lista2/cpp/pascaltrianglerow.cpp
@@ -5,6 +5,11 @@
 
 void pascaltrianglerow::create_row(int n){
   this->row[0] = 1;
+  this->previous_row[0] = 1;
+  for (int i=1; i<n; i++){
+    this->row[i] = 0;
+    this->previous_row[i] = 0;
+  }
   if (n == 0) return;
   this->row[1] = 1;
   if (n == 1) return;
@@ -27,12 +32,6 @@ pascaltrianglerow::pascaltrianglerow(int n){
   this->row_length = n+1;
   this->row = (int*) malloc(sizeof(int)*(n+1));
   this->previous_row = (int*) malloc(sizeof(int)*(n+1));
-  this->row[0] = 1;
-  this->previous_row[0] = 1;
-  for (int i=1; i<n; i++){
-    this->row[i] = 0;
-    this->previous_row[i] = 0;
-  }
   create_row(n);
 }
 
